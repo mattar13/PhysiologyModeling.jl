4b3e5b38a2d231252629218a5a95556ef6dbf906
mattar13 <mtarchick13@gmail.com>
2023-11-08 10:20:40 -0500

    updated imports

--- src/PhysiologyModeling.jl
+++ src/PhysiologyModeling.jl
@@ -30,9 +30,9 @@ export nt_u0, nt_p0
 export extract_p0
 
 include("StarburstModel/Models.jl")
-export SAC_ODE, SAC_PDE 
-export ∇α
-export noise1D
+export SAC_ODE, SAC_PDE, SAC_PDE_STIM 
+export ∇α, DIFFUSION_MODEL, DIFFUSION_NOISE
+export noise1D, noise2D
 
 include("StarburstModel/Mapping.jl")
 export even_map
