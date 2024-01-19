a95d20ac2a9beadabf6b6f5df4e11eaef2ee0632
mattar13 <mtarchick13@gmail.com>
2024-01-18 18:57:33 -0800

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -1,27 +1,46 @@
-#Activate the testing environment
-using Pkg; Pkg.activate("test")
-
+using Revise
+using PhysiologyModeling
+using Pkg; Pkg.activate("test") #Activate the testing environment
 #I think a good next step is to add in the ability to open and compare data versus physiologyical data
 # create a model output here
-using Revise
-using ElectroPhysiology, PhysiologyModeling
+using ElectroPhysiology
+using PhysiologyPlotting, GLMakie
 
+f = Figure(size = (2.5, 2.5))
+ax1 = Axis(f[1,1], 
+     title = "Experiment Plot Test",
+     xlabel = "Time (ms)", 
+     ylabel = "Response (μV)"
+)
 #First try opening some data
-#Also we need to add some of the documentation to ElectroPhysiology.jl
+#find a good data point
+folder = raw"F:\SAC_project_backup_Feller_Lab\SAC-project_ephys_data\01132023-P10\B2-KO-nGFP\cell1_SAC"
+file = "gap_free_voltage_0000.abf"
+path = joinpath(folder, file)
+data = readABF(path, channels = ["I_MTest 1"], stimulus_name = nothing)
+size(data)
+data.t
 
-#1) Run the model using the default settings
-tspan = (0.0, 10e3)
+downsample!(data, 10.0)
+plot_experiment(ax1, data)
+
+display(f)
+#1) Run the model using the default settingstspan = (0.0, 10e3)
 p0 = extract_p0(SAC_p0_dict)
-prob = SDEProblem(SAC_ODE, noise1D, vals_u0, tspan, p0)
+prob = SDEProblem(SAC_ODE, noise1D, vals_u0, (0.0, 300e3), p0)
 @time sol = solve(prob, SOSRI(), reltol = 0.01, abstol = 0.01, progress = true, progress_steps = 1)
+sim_exp = Experiment(sol) #Turn this into an experiment
 
-#Turn this into an experiment
-sim_exp = Experiment(sol)
+f = Figure(size = (2.5, 2.5))
+ax1 = Axis(f[1,1], 
+     title = "Experiment Plot Test",
+     xlabel = "Time (ms)", 
+     ylabel = "Response (μV)"
+)
+plot_experiment(ax1, sim_exp)
 
 
-#Now we can open a experiment of our own
-
-
+#%% This section is testing the GPU 
 #1) Run the model using the default settings
 #I want to use GPUs to improve the speed of the diffusion reaction. 
 
