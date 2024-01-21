1f831923ab391df585befcf9a0ff4349a473bb66
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-01-20 16:56:50 -0800

    added GPU models to make_GPU file

--- src/make_GPU.jl
+++ src/make_GPU.jl
@@ -1,3 +1,12 @@
+mutable struct CellMapGPU{T}
+	xs::Vector{T}
+	ys::Vector{T}
+     radius::Vector{T}
+	connections::CUDA.CUSPARSE.CuSparseMatrixCSC{T, Int32}
+     strength::CUDA.CUSPARSE.CuSparseMatrixCSC{T, Int32}
+     strength_out::Vector{T}
+end
+
 function make_GPU(MAP::CellMap{T}) where T <: Real
 	xs = MAP.xs |> Vector{Float32}
      ys = MAP.ys |> Vector{Float32}
@@ -5,5 +14,10 @@ function make_GPU(MAP::CellMap{T}) where T <: Real
      connections = MAP.connections .|> Float32 |> CUSPARSE.CuSparseMatrixCSC
      strength = MAP.strength .|> Float32 |> CUSPARSE.CuSparseMatrixCSC
      strength_out = MAP.strength_out |> Vector{Float32}
-     return CellMap(xs, ys, radius, connections, strength, strength_out)
+     return CellMapGPU(xs, ys, radius, connections, strength, strength_out)
+end
+
+function SAC_PDE_GPU(du, u, p, t, MAP::CellMapGPU)
+     CUDA.@sync SAC_PDE(du, u, p, t, MAP)
+     return
 end
