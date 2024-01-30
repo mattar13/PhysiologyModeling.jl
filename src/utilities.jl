4d092efa6f94218742b2010c28d5232a11248a06
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-01-29 18:19:00 -0800

    updated ElectroPhysiology experiment

--- src/utilities.jl
+++ src/utilities.jl
@@ -7,8 +7,13 @@ function extend_dims(A,which_dim)
 end
 
 import ElectroPhysiology.Experiment
-function Experiment(sol::SciMLBase.AbstractSciMLSolution)
+function Experiment(sol::SciMLBase.AbstractSciMLSolution; 
+     channels = :vars,
+)
      sol_arr = extend_dims(sol |> Array, 3)
+     if channels == :vars
+          sol_arr = permutedims(sol_arr, (3,2,1))
+     end
      #Enter the Header here
      Experiment(sol.t, sol_arr)
 end
