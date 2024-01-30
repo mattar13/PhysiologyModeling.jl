48ffd214fad9c80682e07a9e746ff9f5f47a48b7
mattar13 <mtarchick13@gmail.com>
2024-01-29 18:14:38 -0800

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -5,7 +5,50 @@ using Pkg; Pkg.activate("test") #Activate the testing environment
 using PhysiologyPlotting, GLMakie
 using BenchmarkTools
 
-#%% Making cAMP a quantity will help with adding dopamine into models I think
+#%% Section A: Testing models against physiology data
+#I think a good next step is to add in the ability to open and compare data versus physiologyical data
+root = raw"E:\Data\Patching"
+file = "2024_01_25_ChAT-RFP_DSGC/Cell1/24125000.abf"
+filename = joinpath(root, file)
+data = readABF(filename)
+import ElectroPhysiology.create_signal_waveform!
+create_signal_waveform!(data, "Analog 0")
+
+
+f = Figure(size = (200, 200))
+ax1 = Axis(f[1,1], 
+     title = "Experiment Plot Test",
+     xlabel = "Time (ms)", 
+     ylabel = "Response (mV)"
+)
+ax2 = Axis(f[1,2])
+
+plot_experiment(ax1, exp; channels = 1)
+plot_experiment(ax1, exp; channels = 2)
+
+#First try opening some data
+#find a good data point
+data = readABF(filename)
+size(data)
+
+
+downsample!(data, 10.0)
+plot_experiment(ax1, data)
+
+display(f)
+#1) Run the model using the default settingstspan = (0.0, 10e3)
+p0 = extract_p0(SAC_p0_dict)
+prob = SDEProblem(SAC_ODE, noise1D, vals_u0, (0.0, 300e3), p0)
+@time sol = solve(prob, SOSRI(), reltol = 0.01, abstol = 0.01, progress = true, progress_steps = 1)
+sim_exp = Experiment(sol) #Turn this into an experiment
+
+f = Figure(size = (2.5, 2.5))
+ax1 = Axis(f[1,1], 
+     title = "Experiment Plot Test",
+     xlabel = "Time (ms)", 
+     ylabel = "Response (μV)"
+)
+plot_experiment(ax1, sim_exp)
 
 # Step 1. Set up all parameters for the ODE
 tspan = (0.0, 120e3)
@@ -42,35 +85,3 @@ f_PDE(du, u, p, t) = SAC_PDE_GPU(du, u, p, t, cell_map)
 prob = SDEProblem(f_PDE, noise2D, u0, tspan, p0)
 @time sol = solve(prob, SOSRI(), reltol = 2e-2, abstol = 2e-2, progress=true, progress_steps=1);
 
-#%% Section A: Testing models against physiology data
-#I think a good next step is to add in the ability to open and compare data versus physiologyical data
-
-f = Figure(size = (2.5, 2.5))
-ax1 = Axis(f[1,1], 
-     title = "Experiment Plot Test",
-     xlabel = "Time (ms)", 
-     ylabel = "Response (μV)"
-)
-#First try opening some data
-#find a good data point
-folder = raw"F:\SAC_project_backup_Feller_Lab\SAC-project_ephys_data\01132023-P10\B2-KO-nGFP\cell1_SAC"
-file = "gap_free_voltage_0000.abf"
-path = joinpath(folder, file)
-data = readABF(path, channels = ["I_MTest 1"], stimulus_name = nothing)
-downsample!(data, 10.0)
-plot_experiment(ax1, data)
-
-display(f)
-#1) Run the model using the default settingstspan = (0.0, 10e3)
-p0 = extract_p0(SAC_p0_dict)
-prob = SDEProblem(SAC_ODE, noise1D, vals_u0, (0.0, 300e3), p0)
-@time sol = solve(prob, SOSRI(), reltol = 0.01, abstol = 0.01, progress = true, progress_steps = 1)
-sim_exp = Experiment(sol) #Turn this into an experiment
-
-f = Figure(size = (2.5, 2.5))
-ax1 = Axis(f[1,1], 
-     title = "Experiment Plot Test",
-     xlabel = "Time (ms)", 
-     ylabel = "Response (μV)"
-)
-plot_experiment(ax1, sim_exp)
