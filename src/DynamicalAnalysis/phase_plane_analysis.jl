8f1aa46eae2b9b902de76c6a0a947c32f4eeb639
mattar13 <mtarchick13@gmail.com>
2024-02-19 15:26:50 -0800

    updated phase plane and finding nullclines

--- src/DynamicalAnalysis/phase_plane_analysis.jl
+++ src/DynamicalAnalysis/phase_plane_analysis.jl
@@ -6,45 +6,66 @@ function meshgrid(x_range::AbstractVector, y_range::AbstractVector)
 end
 
 function phase_plane(prob::SciMLBase.AbstractSciMLProblem, xmap::AbstractVector, ymap::AbstractVector, 
-    x_idx = 1,  y_idx = 2,
+    x_idx = 2,  y_idx = 3,
 )
-    uI = prob.u0
     phase_map = zeros(size(xmap,1), size(ymap,1), 2)
-    du = similar(uI)
     
-    for (idx_x, x) in enumerate(xmap), (idx_y, y) in enumerate(ymap) 
+    #Define this function inside of a function to test the phase plane
+    function test_prob(x, y)
+        uI = prob.u0
+        du = similar(uI)
         uI[[x_idx,y_idx]].= [x, y]
         prob.f(du, uI, prob.p, 0.0)
-        phase_map[idx_x, idx_y, :] = du[[x_idx,y_idx]]
+        return du[[x_idx, y_idx]]
+   end
+
+    for (idx_x, x) in enumerate(xmap), (idx_y, y) in enumerate(ymap) 
+        phase_map[idx_x, idx_y, :] = test_prob(x, y)
     end
     return phase_map
 end
 
-## To be tested
-function find_nullclines(prob::SciMLBase.AbstractSciMLProblem, xmap::AbstractVector, ymap::AbstractVector, 
-    x_idx = 1, y_idx = 2, tolerance = 1e-3
+## In order to do this, we probably need to have xmap and ymap
+function find_nullclines(prob::SciMLBase.AbstractSciMLProblem, xmap::AbstractVector, ymap::AbstractVector; 
+    x_idx = 2, y_idx = 3, xic = -90.0, yic = 0.0
 )
-    # Generate the phase map
-    phase_map = phase_plane(prob, xmap, ymap, x_idx, y_idx)
 
     # Initialize containers for nullclines
-    x_nullcline = []
-    y_nullcline = []
-
-    for (idx_x, x) in enumerate(xmap), (idx_y, y) in enumerate(ymap)
-        # Extract derivatives
-        dx, dy = phase_map[idx_x, idx_y, :]
-
-        # Check for x-nullcline (dx/dt ≈ 0)
-        if abs(dx) < tolerance
-            push!(x_nullcline, (x, y))
+    ysol = fill(NaN, length(xmap))
+    for (ix, x) in enumerate(xmap)
+        println(x)
+        function x_test_prob(y)
+            uI = prob.u0
+            du = similar(uI)
+            uI[x_idx] = x
+            uI[[y_idx]] .= y
+            prob.f(du, uI, prob.p, 0.0)
+            return du[x_idx]
         end
-
-        # Check for y-nullcline (dy/dt ≈ 0)
-        if abs(dy) < tolerance
-            push!(y_nullcline, (x, y))
+        #Solve the test problem
+        nlsol = nlsolve(x_test_prob, [yic])
+        if nlsol.f_converged
+            ysol[ix] = nlsol.zero[1]
+        end
+    end
+    
+    xsol = fill(NaN, length(ymap))
+    for (iy, y) in enumerate(ymap)
+        println()
+        function y_test_prob(x)
+            uI = prob.u0
+            du = similar(uI)
+            uI[[x_idx]] .= x
+            uI[y_idx] = y
+            prob.f(du, uI, prob.p, 0.0)
+            return du[y_idx]
+        end
+        #Solve the test problem
+        nlsol = nlsolve(y_test_prob, [xic])
+        if nlsol.f_converged
+                xsol[iy] = nlsol.zero[1]
         end
     end
 
-    return x_nullcline, y_nullcline
+    return xsol, ysol
 end
