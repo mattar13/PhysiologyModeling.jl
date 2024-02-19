b7284357a0983dec27f847f6060d6d386950c40c
mattar13 <mtarchick13@gmail.com>
2024-02-19 09:59:46 -0800

    updated running Inhibitory and Excitatory models

--- test/SAC_model_tests/runInhExcModel.jl
+++ test/SAC_model_tests/runInhExcModel.jl
@@ -7,20 +7,28 @@ using PhysiologyPlotting, GLMakie
 
 #%%Run the ExcInc model according to this funciton
 tspan = (0.0, 1000.0)
-dt = 0.001
+dt = 0.1
 tstops = tspan[1]:dt:tspan[end]
-input_wave = map(t -> gaussian(t), tstops)
-InhExc_p0_dict["g_Exc"] = 1.0 #Set this experimentally
-InhExc_p0_dict["g_Inh"] = 1.0 #Set this experimentally
+gE_input = rand(size(tstops))
+gI_input = rand(size(tstops))
+
+using MAT
+
+file = matopen(raw"test\SAC_model_tests\eWT_PD.mat")
+datafile = read(file)
+
 p0 = extract_dict(InhExc_p0_dict, InhExc_p0_keys)
-u0 = [0.0, 0.0]
+u0 = [0.0, 0.0, 0.0]
 prob = ODEProblem(InhExcModel, u0, tspan, p0)
 @time sol = solve(prob, tstops = tstops, progress = true, progress_steps = 1)
 
 fInhExc = Figure(size = (400, 400))
-ax1 = Axis(fInhExc[1,1])
-ax2 = Axis(fInhExc[2,1])
+ax_v = Axis(fInhExc[1,1])
+ax_gE = Axis(fInhExc[2,1])
+ax_gI = Axis(fInhExc[3,1])
 Time = sol.t
-lines!(ax1, Time, map(t -> sol(t)[1], Time))
-lines!(ax2, Time, map(t -> sol(t)[2], Time))
+lines!(ax_v, Time, map(t -> sol(t)[1], Time))
+lines!(ax_gE, Time, map(t -> sol(t)[2], Time))
+lines!(ax_gI, Time, map(t -> sol(t)[2], Time))
+
 display(fInhExc)
