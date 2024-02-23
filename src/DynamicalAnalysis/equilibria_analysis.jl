7a411b97685e85115019ccac9d5222db6bc46ac5
mattar13 <mtarchick13@gmail.com>
2024-02-23 14:18:02 -0800

    added equilibria stability analysis

--- src/DynamicalAnalysis/equilibria_analysis.jl
+++ src/DynamicalAnalysis/equilibria_analysis.jl
@@ -74,22 +74,22 @@ function find_fixed_points(prob::SciMLBase.AbstractSciMLProblem, xmap::AbstractV
     )
     xlims = (xmap[1], xmap[end])
     ylims = (ymap[1], ymap[end])
-    fixed_points = Tuple[] #This stores all of the fixed points
-    function test_prob(x)
-        uI = prob.u0
-        du = similar(uI)
-        uI[[x_idx,y_idx]].= x
-        prob.f(du, uI, prob.p, 0.0)
-        return du[[x_idx, y_idx]]
+    fixed_points = Vector[] #This stores all of the fixed points
+    function test_prob(ux)
+        du = similar(ux)
+        prob.f(du, ux, prob.p, 0.0) #solve this problem for zero
+        return du       
     end
     for (idx_x, x) in enumerate(xmap), (idx_y, y) in enumerate(ymap)
         #Iterate through the y range looking for stable points
+        uI = copy(prob.u0)
+        uI[[x_idx, y_idx]] .= [x, y]
         if verbose
             println("Checking parameter")
             println("var 1 = $x")
             println("var 2 = $y")
         end
-        res = nlsolve(test_prob, [x,y])
+        res = nlsolve(test_prob, uI)
         #Check to see if the nlsolve failed
         
         #If any of the results are out of bounds then we have to cut it off
@@ -99,10 +99,10 @@ function find_fixed_points(prob::SciMLBase.AbstractSciMLProblem, xmap::AbstractV
             print("f = 0: ")
             println(res.zero)
         end
-        var1_inbounds = xlims[1] < res.zero[1] <= xlims[2]
-        var2_inbounds = ylims[1] < res.zero[2] <= ylims[2]
+        var1_inbounds = xlims[1] < res.zero[x_idx] <= xlims[2]
+        var2_inbounds = ylims[1] < res.zero[y_idx] <= ylims[2]
         if var1_inbounds && var2_inbounds
-            fixed_points_check = map(x -> round(x, digits=precision), res.zero) |> Tuple
+            fixed_points_check = map(x -> round(x, digits=precision), res.zero)
             if fixed_points_check ∉ fixed_points && res.f_converged
                 if verbose
                     println(res.f_converged)
@@ -124,6 +124,31 @@ function find_fixed_points(prob::SciMLBase.AbstractSciMLProblem, xmap::AbstractV
     return fixed_points
 end
 
+function equilibrium_stability(prob::SciMLBase.AbstractSciMLProblem, fixed_point; x_idx = 2, y_idx = 3)
+    function test_prob(ux)
+        du = similar(ux)
+        prob.f(du, ux, prob.p, 0.0) #solve this problem for zero
+        return du       
+    end
+    test_jac = ForwardDiff.jacobian(test_prob, fixed_point)[[x_idx, y_idx], [x_idx, y_idx]]
+    ev = eigvals(test_jac)
+    if sign(real(ev[1])) != sign(real(ev[2])) #If the signs of the eigen values are not equal, the equilbria is unstable   
+        return :Saddle
+    elseif imag(ev[1]) != 0.0 #This detects whether the equilibria is a focus equilibria
+        if real(ev[1]) >= 0.0
+            return :UnstableFocus
+        elseif real(ev[1]) < 0.0
+            return :StableFocus
+        end
+    else #If it is not a saddle and not a focus, then it is just a plain equilibria
+        if ev[1] >= 0.0
+            return :Unstable
+        elseif ev[1] < 0.0
+            return :Stable
+        end
+    end 
+
+end
 
 #%%
 function find_equilibria(prob::SciMLBase.AbstractSciMLProblem, xlims::Tuple, ylims::Tuple;
