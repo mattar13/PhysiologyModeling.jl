36c1a04959677fe0d58073838cb0f7bc149ce128
mattar13 <mtarchick13@gmail.com>
2024-02-23 14:21:36 -0800

    saved equilibria analysis

--- src/DynamicalAnalysis/equilibria_analysis.jl
+++ src/DynamicalAnalysis/equilibria_analysis.jl
@@ -124,7 +124,7 @@ function find_fixed_points(prob::SciMLBase.AbstractSciMLProblem, xmap::AbstractV
     return fixed_points
 end
 
-function equilibrium_stability(prob::SciMLBase.AbstractSciMLProblem, fixed_point; x_idx = 2, y_idx = 3)
+function find_equilibria(prob::SciMLBase.AbstractSciMLProblem, fixed_point; x_idx = 2, y_idx = 3)
     function test_prob(ux)
         du = similar(ux)
         prob.f(du, ux, prob.p, 0.0) #solve this problem for zero
@@ -148,109 +148,4 @@ function equilibrium_stability(prob::SciMLBase.AbstractSciMLProblem, fixed_point
         end
     end 
 
-end
-
-#%%
-function find_equilibria(prob::SciMLBase.AbstractSciMLProblem, xlims::Tuple, ylims::Tuple;
-    x_vars = 2, y_vars = 3, 
-    equilibrium_resolution = 10,
-    precision = 2, check_min = 1e-5, verbose = false
-)
-    stable = Array{Array{Float64}}([])
-    unstable = Array{Array{Float64}}([])
-    saddle = Array{Array{Float64}}([])
-    unstable_focus = Array{Array{Float64}}([])
-    stable_focus = Array{Array{Float64}}([])
-    storage = Array{Array{Float64}}([])
-    xmap = LinRange(xlims[1], xlims[2], equilibrium_resolution)
-    ymap = LinRange(ylims[1], ylims[2], equilibrium_resolution)
-    function df(ux) 
-        #println("at least we made it here")
-        du = similar(ux)
-        prob.f(du, ux, prob.p, 0.0) #solve this problem for zero
-        return du           
-    end
-
-    for (idx_x, x) in enumerate(xmap) for (idx_y, y) in enumerate(ymap)
-            #Iterate through the y range looking for stable points
-            if verbose
-                println("Checking parameter")
-                println("var 1 = $x")
-                println("var 2 = $y")
-            end
-            uI = copy(prob.u0)
-            uI[[x_vars, y_vars]] .= [x, y]
-        
-            if verbose
-                println("Condition to check = $uI")
-            end
-        
-            res = nlsolve(df, uI)
-            #If any of the results are out of bounds then we have to cut it off
-            if verbose
-                print("Results of the nlsolve for uI: ")
-                #println(res)
-                print("f = 0: ")
-                println(res.zero)
-            end
-        
-            #We want to remove all cases where the result is out of bounds
-            var1_inbounds = xlims[1] < res.zero[var_idx[1]] <= xlims[2]
-            var2_inbounds = ylims[1] < res.zero[var_idx[2]] <= ylims[2]
-
-            if var1_inbounds && var2_inbounds #We want to make sure the equilibria is not out of bounds
-                equilibria = map(x -> round(x, digits=precision), res.zero)
-                check = df(res.zero)
-                if verbose
-                    print("Checking the solution and it's proximity to zero: dUᵢ = ")
-                    println(check)
-                end
-                if equilibria in storage
-                    nothing
-                elseif any(isnan, res.zero)
-                    nothing
-                elseif check[1] > check_min || check[2] > check_min
-                    nothing
-                else
-                    push!(storage, equilibria)
-                    #push!(eq_dict[:all], res.zero)
-                    #If the equilibria is not in the stable or unstable points
-                    #Test it's stability
-                    J_mat = ForwardDiff.jacobian(df, res.zero)[[var_idx...], [var_idx...]]
-                    ev = eigvals(J_mat)
-            
-                    if sign(real(ev[1])) != sign(real(ev[2]))
-                        #If the sign of the eigenvalues are opposite, then it is a unstable saddle
-                        #println("Saddle")
-                        push!(saddle, res.zero)
-                    else
-                        if imag(ev[1]) != 0 #This detects whether or not the equilibria is a focus
-                            if real(ev[1]) >= 0.0
-                                #println("Unstable focus")
-                                push!(unstable_focus, res.zero)
-                            else
-                                #println("Unstable focus")
-                                push!(stable_focus, res.zero)
-                            end
-                        else
-                            if ev[1] > 0.0
-                                push!(unstable, res.zero)
-                            else
-                                push!(stable, res.zero)
-                            end
-                        end
-                    end
-                end
-            else
-                if !var1_inbounds && verbose
-                    println("Variable $(vars[1]) is out of bounds at $(res.zero[var_idx[1]]) will be skipped")
-                end
-
-                if !var2_inbounds && verbose
-                    println("Variable $(vars[2]) is out of bounds at $(res.zero[var_idx[2]]) will be skipped")
-                end
-            end
-        end
-    end
-    return equilibria_object{Float64}(stable, unstable, saddle, unstable_focus, stable_focus)
 end
