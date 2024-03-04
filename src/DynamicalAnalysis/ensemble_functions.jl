6643a29867f76dd129700ddcec858032a77d56f7
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-03-04 00:52:03 -0800

    updated ensemble functions

--- src/DynamicalAnalysis/ensemble_functions.jl
+++ src/DynamicalAnalysis/ensemble_functions.jl
@@ -1,10 +1,10 @@
-function OneVarEnsembleProb(prob, i, repeat, params; idx = 1)
+function OneVarEnsembleProb(prob, i, repeat, params)
      pI = prob.p
      pI[idx] = params[i]
      remake(prob, p = pI)
 end
 
-function InitialCond_Param_EnsembleProb(prob, i, repeat, ics, params)
+function InitialCond_Param_EnsembleProb(prob, i, repeat, ics, params; idx = 1)
      pI = prob.p
      pI[idx] = params[i]
      remake(prob, p = pI, u0 = ics[:, i])
