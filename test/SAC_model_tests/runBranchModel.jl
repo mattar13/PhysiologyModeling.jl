0e4fe6e5038e81d58a7bb72e226dd392da647c36
mattar13 <mtarchick13@gmail.com>
2024-03-04 17:21:45 -0800

    updated branch model

---
+++ test/SAC_model_tests/runBranchModel.jl
@@ -0,0 +1,5 @@
+using Revise
+using ElectroPhysiology, PhysiologyModeling
+using Pkg; Pkg.activate("test") #Activate the testing environment
+using PhysiologyPlotting, GLMakie
+#This uses all of the same auxillary equations
