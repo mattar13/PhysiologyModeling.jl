f7eb0138d8de386156af9e521fac730f301f8d6b
mattar13 <mtarchick13@gmail.com>
2024-03-07 17:41:07 -0800

    updated examples

--- test/SAC_model_tests/runSDEModel.jl
+++ test/SAC_model_tests/runSDEModel.jl
@@ -1,19 +1,22 @@
 using Revise
 using ElectroPhysiology
 using PhysiologyModeling
+using DiffEqCallbacks
 using Pkg; Pkg.activate("test")
 using PhysiologyPlotting
 using GLMakie
 
 #%%=================================[Solving a single SDE for tspan]=================================#
-tspan = (0.0, 100.0)
+tspan = (0.0, 100e3)
 #Extract and modify parameters
 p0_dict = SAC_p0_dict()
 p0_dict["g_GABA"] = 0.0
 p0_dict["g_ACh"] = 0.0
+p0_dict["g_GLUT"] = 0.0 #This ensures the 
 p0 = extract_p0(p0_dict)
 #Extract and modify initial conditions
 u0_dict = SAC_u0_dict()
+u0_dict["g"] = 0.5
 u0 = extract_u0(u0_dict)
 #Set up the problem
 prob_func(du, u, p, t) = SAC_ODE(du, u, p, t)
@@ -33,9 +36,11 @@ ax7 = Axis(fSDE[3,2], title = "TREK (Bt)")
 
 ax8 = Axis(fSDE[1,3], title = "ACh (Et)")
 ax9 = Axis(fSDE[2,3], title = "GABA (It)")
+ax10 = Axis(fSDE[3,3], title = "Glutamate (Gt)")
+ax11 = Axis(fSDE[4,3], title = "Gq (Qt)")
 
-ax10 = Axis(fSDE[1,4], title = "Noise (Wt)")
-ax11 = Axis(fSDE[2,4], title = "I_ext (pA)")
+ax12 = Axis(fSDE[1,4], title = "Noise (Wt)")
+ax13 = Axis(fSDE[2,4], title = "I_ext (pA)")
 
 Time = sol.t
 lines!(ax1, Time, map(t -> sol(t)[2], Time))
@@ -48,7 +53,9 @@ lines!(ax7, Time, map(t -> sol(t)[8], Time))
 lines!(ax8, Time, map(t -> sol(t)[9], Time))
 lines!(ax9, Time, map(t -> sol(t)[10], Time))
 lines!(ax10, Time, map(t -> sol(t)[11], Time))
-lines!(ax11, Time, map(t -> sol(t)[1], Time))
+lines!(ax11, Time, map(t -> sol(t)[12], Time))
+lines!(ax12, Time, map(t -> sol(t)[13], Time))
+lines!(ax13, Time, map(t -> sol(t)[1], Time))
 
 display(fSDE)
 save("test/SAC_model_tests/data/SDESol_Ek_leak.png", fSDE)
@@ -122,21 +129,31 @@ end
 display(fSDE)
 
 #%%=================================[Solving a single SDE by adding mGluR6 into the equation]=================================#
-tspan = (0.0, 100.0)
+tspan = (0.0, 10e3)
+dt = 1.0
+
+tseries = tspan[1]:dt:tspan[2]
+#create a glutamate pulse
 #Extract and modify parameters
 p0_dict = SAC_p0_dict()
-p0 = extract_p0(p0_dict, mode = :GLUTAMATE)
-p0_dict["g_GLUT"]
+p0_dict["g_GABA"] = 0.0
+p0_dict["g_ACh"] = 0.0
+p0 = extract_p0(p0_dict)
 #Extract and modify initial conditions
 u0_dict = SAC_u0_dict()
-u0 = extract_u0(u0_dict, mode = :GLUTAMATE)
+u0_dict["g"] = 0.0
+u0 = extract_u0(u0_dict)
+
+#create a callback for the glutamate pulse
+affect!(integrator) = integrator.u[11] = gauss_pulse(integrator.t; t0 = 8000.0, spread = 500.0) + gauss_pulse(integrator.t; t0 = 2000.0, spread = 500.0)
+cb = PresetTimeCallback(tseries, affect!)
+
 #Set up the problem
-prob_func(du, u, p, t) = SAC_ODE_GLUT(du, u, p, t)
+prob_func(du, u, p, t) = SAC_ODE(du, u, p, t)
 prob = SDEProblem(prob_func, noise1D, u0, tspan, p0)
-@time sol = solve(prob, SOSRI(), reltol = 2e-2, abstol = 2e-2, progress = true, progress_steps = 1)
+@time sol = solve(prob, SOSRI(), tstops = tseries, callback = cb, reltol = 2e-2, abstol = 2e-2, progress = true, progress_steps = 1)
 
-#====================================[Plot the solution]====================================#
-
+# ====================================[Plot the solution]====================================#
 fSDE = Figure(size = (1800, 800))
 ax1 = Axis(fSDE[1,1], title = "Voltage (Vt)")
 ax2 = Axis(fSDE[2,1], title = "K Repol. (Nt)")
@@ -147,11 +164,13 @@ ax5 = Axis(fSDE[1,2], title = "Calcium (Ct)")
 ax6 = Axis(fSDE[2,2], title = "cAMP (At)")
 ax7 = Axis(fSDE[3,2], title = "TREK (Bt)")
 
-ax8 = Axis(fSDE[1,3], title = "Glutamate (Gt)")
-ax9 = Axis(fSDE[2,3], title = "Gq (Qt)")
+ax8 = Axis(fSDE[1,3], title = "ACh (Et)")
+ax9 = Axis(fSDE[2,3], title = "GABA (It)")
+ax10 = Axis(fSDE[3,3], title = "Glutamate (Gt)")
+ax11 = Axis(fSDE[4,3], title = "Gq (Qt)")
 
-ax10 = Axis(fSDE[1,4], title = "Noise (Wt)")
-ax11 = Axis(fSDE[2,4], title = "I_ext (pA)")
+ax12 = Axis(fSDE[1,4], title = "Noise (Wt)")
+ax13 = Axis(fSDE[2,4], title = "I_ext (pA)")
 
 Time = sol.t
 lines!(ax1, Time, map(t -> sol(t)[2], Time))
@@ -164,6 +183,9 @@ lines!(ax7, Time, map(t -> sol(t)[8], Time))
 lines!(ax8, Time, map(t -> sol(t)[9], Time))
 lines!(ax9, Time, map(t -> sol(t)[10], Time))
 lines!(ax10, Time, map(t -> sol(t)[11], Time))
-lines!(ax11, Time, map(t -> sol(t)[1], Time))
+lines!(ax11, Time, map(t -> sol(t)[12], Time))
+lines!(ax12, Time, map(t -> sol(t)[13], Time))
+lines!(ax13, Time, map(t -> sol(t)[1], Time))
 
 display(fSDE)
+save("test/SAC_model_tests/data/SDESol_GLUT.png", fSDE)
