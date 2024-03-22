714fc6f8609413eac818d094688323ad50d809c1
mattar13 <mtarchick13@gmail.com>
2024-03-22 11:17:43 -0700

    changed some documentation

--- test/SAC_model_tests/runPDEModel.jl
+++ test/SAC_model_tests/runPDEModel.jl
@@ -11,7 +11,7 @@ using LinearAlgebra
 #Try importing some other solver methods
 import .PhysiologyModeling: CVODE_BDF, ring
 import .PhysiologyModeling.DifferentialEquations: ImplicitRKMil, SKenCarp
-#%%=[Run branch generation]__________________________________________________________________________________#
+#%%=[Create the cell maps]__________________________________________________________________________________#
 
 #1) determine the domains and spacing of cells. 
 domain_x = (xmin, xmax) = (0.0, 5.0) #This is a simulation for a retina 5mm in diameter
