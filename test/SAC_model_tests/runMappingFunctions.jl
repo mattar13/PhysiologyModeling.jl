ec6aff4dff37d72f2e6ca44f7a17b35f6ac0b877
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-04-14 11:09:31 -0700

    updated example map functions

--- test/SAC_model_tests/runMappingFunctions.jl
+++ test/SAC_model_tests/runMappingFunctions.jl
@@ -19,29 +19,53 @@ for ix in axes(coords, 1), iy in axes(coords,2)
 end
 dist_func1(d) = ring_circle_overlap_area(d; density = 0.01, r_inner = 0.1, r_outer = 0.18, r_circle = 0.18)
 dist_func2(d) = ring(d; density = 0.01, max_dist = 0.18, slope = 0.025)
-
+dist_func3(d) = d
 strengths1 = dist_func1.(distances)
 strengths2 = dist_func2.(distances)
+strengths3 = dist_func3.(distances)
 distances
 
+# [Plot the function] _______________________________________________________________#
+fig1 = Figure(size = (1200, 300))
+ax1a = Axis(fig1[1,1])
+ax1b = Axis(fig1[1,2])
+ax1c = Axis(fig1[1,3])
+ax1d = Axis(fig1[1,4])
+heatmap!(ax1a, xrng, yrng, distances)
+heatmap!(ax1b, xrng, yrng, strengths1)
+heatmap!(ax1c, xrng, yrng, strengths2)
+heatmap!(ax1d, xrng, yrng, strengths3)
+scatter!(ax1a, origin); scatter!(ax1b, origin); scatter!(ax1c, origin)
+save("test/SAC_model_tests/data/MappingFunc.png", fig1)
+
 #%% [Drawing random cell maps] ________________________________________________________#
-xs, ys = create_random_map(300)
+#1) determine the domains and spacing of cells. 
+domain_x = (xmin, xmax) = (0.0, 5.0) #This is a simulation for a retina 5mm in diameter
+domain_y = (ymin, ymax) = (0.0, 5.0)
+dx = dy = 0.05 #Mean distribution is 40-50 micron (WR taylor et al)
 
+#2) create a random distribution of cells and their radii
+#The density of SACs in the retina is around 1200 per mm2. So if we have 5mm2 1200 * 5 = 6000
+n_cells = 600 #Really pushing the model
+xs, ys = create_random_map(n_cells, dx = dy = 0.01, xmax = 2.0, ymax = 2.0)
 
-#%% [Plot the function] _______________________________________________________________#
-fmap = Figure(size = (900, 600))
-axA1 = Axis(fmap[1,1])
-axA2 = Axis(fmap[1,2])
-axA3 = Axis(fmap[1,3])
-heatmap!(axA1, xrng, yrng, distances)
-heatmap!(axA2, xrng, yrng, strengths1)
-heatmap!(axA3, xrng, yrng, strengths2)
-scatter!(axA1, origin); scatter!(axA2, origin); scatter!(axA3, origin)
+connection_list = connect_neighbors_radius(xs, ys, 0.2)
+connection_list = connection_list[findall(c -> c[3] > 0.0, connection_list)]
+connections = connection_matrix(connection_list, m = length(xs), n = length(ys))
+dist_func1(d) = ring(d; density = 0.01, max_dist = 0.18, slope = 0.025);
+cell_map_CPU = CellMap(xs, ys, connections; distance_function = dist_func1);
+cell_map_CPU.strength[2,:]
 
-axB1 = Axis(fmap[2,1])
-axB2 = Axis(fmap[2,2])
-axB3 = Axis(fmap[2,3])
-
-display(fmap)
-#%%
-save("test/SAC_model_tests/data/DistanceFunc.png", f_DIST)
+fig2 = Figure(size = (400,400))
+ax2a = Axis(fig2[1,1], title = "Coordinates", xlabel = "nx", ylabel = "ny")
+#ax2b = Axis(fig2[2,1], title = "Calcium ROIs", xlabel = "time (ms)", ylabel = "Ct")
+#ax2c = Axis(fig2[3,1], title = "Voltage Traces", xlabel = "time (ms)", ylabel = "Vt (mV)")
+rows, cols, data = findnz(cell_map_CPU.strength)
+for i in eachindex(rows)
+    r = rows[i]
+    c = cols[i]
+    d = data[i]
+    lines!(ax2a, [xs[r], xs[c]], [ys[r], ys[c]], colormap = :viridis, color = [d], colorrange = (minimum(data), maximum(data)))
+end
+sctV = scatter!(ax2a, xs, ys, markersize = 15.0)
+display(fig2)
