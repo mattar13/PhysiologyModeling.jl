9a860abdafa970975074ed470db7684798888327
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-04-25 01:12:32 -0700

    updated PDE model

--- test/Modeling/runPDEModel.jl
+++ test/Modeling/runPDEModel.jl
@@ -9,33 +9,25 @@ using SparseArrays
 using LinearAlgebra
 
 
-#Try importing some other solver methods
-import .PhysiologyModeling: CVODE_BDF, ring
-import .PhysiologyModeling.DifferentialEquations: ImplicitRKMil, SKenCarp
-
-save_fn = "D:/Data/Analysis/2024_02_12_WTCell1_MODEL.mp4"
-
 #%% [Create the cell maps]__________________________________________________________________________________#
+save_fn = "Modeling/Results/2024_02_12_WTCell1_MODEL.mp4"
 
 #1) determine the domains and spacing of cells. 
-domain_x = (xmin, xmax) = (0.0, 5.0) #This is a simulation for a retina 5mm in diameter
-domain_y = (ymin, ymax) = (0.0, 5.0)
+domain_x = (xmin, xmax) = (-2.0, 2.0) #This is a simulation for a retina 5mm in diameter
+domain_y = (ymin, ymax) = (-2.0, 2.0)
 dx = dy = 0.05 #Mean distribution is 40-50 micron (WR taylor et al)
 
 #2) create a random distribution of cells and their radii
 #The density of SACs in the retina is around 1200 per mm2. So if we have 5mm2 1200 * 5 = 6000
-n_cells = 300 #Really pushing the model
+n_cells = 600 #Really pushing the model
 xs, ys = create_random_map(n_cells, 
      xmin = xmin, dx = dx, xmax = xmax, 
      ymin = ymin, dy = dy, ymax = ymax
 )
-connection_list = connect_neighbors_radius(xs, ys, 0.2)
+connection_list = connect_neighbors_radius(xs, ys, 0.18, self_connecting = false)
 connections = connection_matrix(connection_list, m = length(xs), n = length(ys))
-dist_func1(d) = ring_circle_overlap_area(d; density = 0.1, r_inner = 0.1, r_outer = 0.2, r_circle = 0.2);
-cell_map_CPU = CellMap(xs, ys, connections; distance_function = dist_func1);
-
-#only run GPU on big computer
-cell_map = cell_map_CPU |> make_GPU
+dist_func(d) = ring_circle_overlap_area(d; density = 0.50, r_inner = 0.1, r_outer = 0.15, r_circle = 0.18);
+cell_map = CellMap(xs, ys, connections; distance_function = dist_func) |> make_GPU;
 
 #[run the model]____________________________________________________________________________#
 p0_dict = SAC_p0_dict()
@@ -53,12 +45,16 @@ prob = SDEProblem(f_PDE, noise2D, u0, tspan, p0)
 @time sol = solve(prob, 
      #SOSRI(), #This seems to be the best solver option
      SOSRA(),
-     reltol =  2e-2, abstol = 2e-2, 
+     reltol =  0.1, abstol = 0.1, 
      progress=true, progress_steps=1)
 #save("data.jld", "initial_cond", sol[end])
  
 CUDA.allowscalar(true) #allow GPU operations to be offloaded to CPU 
 Time = sol.t[1]:10:sol.t[end]
+
+start_time = 35e3
+end_time = 50e3
+Time = start_time:10:end_time
 vt = hcat(map(t -> sol(t)[:,2], Time)...)|>Array
 nt = hcat(map(t -> sol(t)[:,3], Time)...)|>Array
 mt = hcat(map(t -> sol(t)[:,4], Time)...)|>Array
@@ -77,18 +73,18 @@ ax1b = Axis(fig1[1,2], title = "Calcium ROIs", xlabel = "time (ms)", ylabel = "C
 ax1c = Axis(fig1[2,2], title = "Voltage Traces", xlabel = "time (ms)", ylabel = "Vt (mV)")
 
 rowsize!(fig1.layout, 1, Relative(1/2)) #Make the cell plot larger
-sctV = scatter!(ax1a, xs, ys, color = sol(0.0)[:,6]|>Array, colorrange = (0.0, maximum(ct)), markersize = 20.0)
+sctV = scatter!(ax1a, xs, ys, color = sol(start_time)[:,6]|>Array, colorrange = (0.0, maximum(ct)), markersize = 20.0)
 for n in 1:n_cells
      lines!(ax1b, Time, ct[n, :])
      lines!(ax1c, Time, vt[n, :])
 end
-tickerb = vlines!(ax1b, [0.0])
-tickerc = vlines!(ax1c, [0.0])
+tickerb = vlines!(ax1b, [start_time])
+tickerc = vlines!(ax1c, [start_time])
 
 display(fig1)
 
-n_frames = 1000
-animate_t = LinRange(0.0, sol.t[end], n_frames)
+n_frames = 200
+animate_t = LinRange(start_time, end_time, n_frames)
 dt = animate_t[2] - animate_t[1]
 fps = round(Int64, (1/dt) * 1000)
 
