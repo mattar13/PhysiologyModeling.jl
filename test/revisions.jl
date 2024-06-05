e009a76631641611a42afa76876e445efd14ea70
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-06-05 00:05:10 -0700

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -2,41 +2,12 @@ using Revise
 using ElectroPhysiology
 using PhysiologyModeling
 
-#3 Goals
-#GOAL) Analyzing the model
+import PhysiologyModeling.calculate_dendrogram_distance
+#%%=[Run branch generation]__________________________________________________________________________________#
+radial = 5
+branches = 2
+layers = 5
 
-
-
-
-
-
-
-
-#GOAL) Testing models against physiology data
-
-#%% GOAL: Testing models against physiology data
-#I think a good next step is to add in the ability to open and compare data versus physiologyical data
-
-
-#=[Solving a single SDE for tspan]==========================================#
-#Extract and modify initial conditions
-u0_dict = SAC_u0_dict()
-u0 = extract_u0(u0_dict)
-
-#Specify the timespan
-tspan = (0.0, 300e3)
-
-#Set the stimulus parameters
-save_fn = "D:/Data/Analysis/2024_02_12_WT_Cell1_IC.png"
-p0_dict = SAC_p0_dict() #Extract parameters
-p0_dict["g_GABA"] = 0.0 #Null GABA conductance
-p0_dict["g_ACh"] = 0.0 #Null ACh conductance
-p0 = extract_p0(p0_dict)
-
-#Set up the problem
-prob = SDEProblem(SAC_ODE, noise1D, u0, tspan, p0)
-@time sol = solve(prob, SOSRI(), reltol = 2e-2, abstol = 2e-2, progress = true, progress_steps = 1)
-
-exp = Experiment(sol, dt = 1.0)
-exp.t
-
+xs1, ys1, connection_list1 = create_dendrogram_map(radial, branches, layers; origin = (0.0, 0.0))
+xs2, ys2, connection_list1 = create_dendrogram_map(radial, branches, layers; origin = (0.0, 2.0))
+connections = connection_matrix(connection_list, m = length(xs), n = length(ys))
