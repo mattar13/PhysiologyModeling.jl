4ea086f160274f75a1ddffd250f72a788cada2b3
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-08-19 23:29:46 -0700

    added mapping function for better connectivity

---
+++ test/runMappingFunctions.jl
@@ -0,0 +1,89 @@
+using ElectroPhysiology, PhysiologyModeling
+using Pkg; Pkg.activate("test")
+using PhysiologyPlotting, GLMakie
+
+import PhysiologyModeling: euclidean_distance, calculate_linear_bias, find_angle
+
+#%% [Eventually we want to generalize the mapping function] ____________________________________________#
+origin = (0.0, 0.0)
+xs = LinRange(-100.0, 100.0, 1000)
+ys = LinRange(-100.0, 100.0, 1000)
+coords = Iterators.product(xs, ys) |> collect
+
+rinc_str = zeros(size(coords)...)
+ring_str = zeros(size(coords)...)
+cons_str = zeros(size(coords)...)
+angles = zeros(size(coords)...)
+dist_func1(p1, p2) = RING_CIRC(p1, p2; r_inner = 40.0, r_outer = 70.0, r_circle = 70.0) 
+dist_func2(p1, p2) = RING_CIRC(p1, p2; density = 0.05, r_inner = 40.0, r_outer = 70.0, r_circle = 70.0)
+
+angle_bias = 180
+for ix in axes(coords, 1), iy in axes(coords,2)
+    bias = calculate_exponential_bias(find_angle(origin, coords[ix, iy]), 90.0, decay_rate = 0.03)
+    rinc_str[ix, iy] = dist_func1(origin, coords[ix, iy])
+    ring_str[ix, iy] = dist_func2(origin, coords[ix, iy]) * bias
+end
+
+# [Plot the function] _______________________________________________________________#
+fig1 = Figure(size = (600, 500))
+ax1a = Axis(fig1[1,1], title = "RingCirc")
+ax1b = Axis(fig1[1,2], title = "RingCircBias")
+
+ax2a = Axis(fig1[3,1], xlabel = "Vert. Distance (mm)", ylabel = "Strength")
+ax2b = Axis(fig1[3,2], xlabel = "Vert. Distance (mm)", ylabel = "Strength")
+
+hma = heatmap!(ax1a, xs, ys, rinc_str)
+hmb = heatmap!(ax1b, xs, ys, ring_str)
+
+lines!(ax2a, xs, sum(rinc_str, dims = 2)[:,1], color = :black)
+lines!(ax2b, xs, sum(ring_str, dims = 2)[:,1], color = :black)
+
+scatter!(ax1a, origin); scatter!(ax1b, origin)
+Colorbar(fig1[2, 1], hma, vertical = false, label = "Fluorescence (px)")
+Colorbar(fig1[2, 2], hmb, vertical = false, label = "Fluorescence (px)")
+rowsize!(fig1.layout, 2, 0.12)
+rowsize!(fig1.layout, 3, 100.00)
+display(fig1)
+
+#save("Modeling/Results/MappingFunc.png", fig1)
+
+#%% [Changing how coordinates are determined]
+#1) determine the domains and spacing of cells. 
+domain_x = (xmin, xmax) = (-5.0, 5.0) #This is a simulation for a retina 5mm in diameter
+domain_y = (ymin, ymax) = (-5.0, 5.0)
+dx = dy = 0.05 #Mean distribution is 40-50 micron (WR taylor et al)
+
+#2) create a random distribution of cells and their radii
+#The density of SACs in the retina is around 1200 per mm2. So if we have 5mm2 1200 * 5 = 6000
+n_cells = 600 #Really pushing the model
+xs, ys = create_random_map(n_cells, 
+     xmin = xmin, dx = dx, xmax = xmax, 
+     ymin = ymin, dy = dy, ymax = ymax
+)
+connection_list = connect_neighbors_radius(xs, ys, 0.18, self_connecting = false)
+connections = connection_matrix(connection_list, m = length(xs), n = length(ys))
+
+#Plot your bias functions
+bias_func(p1, p2) = calculate_exponential_bias(find_angle(p1, p2), 90.0)
+dist_func(p1, p2) = RING_CIRC(p1, p2) * bias_func(p1, p2)
+cells = CellMap(xs, ys, connections, distance_function = dist_func);
+
+#Reset the first point at each spot 
+fig2 = Figure(size = (800, 800))
+ax1 = Axis(fig2[1,1], 
+    xlabel = "X distance", ylabel = "Y distance"
+)
+scatter!(ax1, (0.0, 0.0), color = :blue, markersize = 20.0)
+
+rows, cols, data = findnz(cells.strength)
+for i in eachindex(rows)
+    r = rows[i]
+    c = cols[i]
+    d = data[i]
+    p1 = (xs[r], ys[r])# .- p2
+    p2 = (xs[c], ys[c]) .- p1
+    scatter!(ax1, p2, color = [d], colorrange = (0.0, maximum(values)), colormap = :viridis, markersize = 50.0)
+end
+
+display(fig2)
+save("test/Modeling/Results/ScatterMappingFunc.png", fig2)
--- test/runSDEModel.jl
+++ test/runSDEModel.jl
@@ -12,9 +12,6 @@ u0 = extract_u0(u0_dict)
 
 #Specify the timespan
 tspan = (0.0, 300e3)
-dt = 1.0
-tseries = tspan[1]:dt:tspan[2]
-
 #Set the stimulus parameters
 save_fn = "D:/Data/Analysis/2024_02_12_WT_Cell1_IC.png"
 p0_dict = SAC_p0_dict() #Extract parameters
@@ -24,18 +21,16 @@ p0_dict["g_ACh"] = 0.0 #Null ACh conductance
 p0 = extract_p0(p0_dict)
 
 #create a callback for the glutamate pulse
-function affect!(integrator; t1 = 0.0, dtpulse = 2500.0, tend = 10e3)
-    dt = 0.0
-    for t0 in t1:dtpulse:tend
-        dt += gauss_pulse(integrator.t; t0 = t0, peak_amp = 0.05)
-    end
-    integrator.u[11] = dt
-end
-cb = PresetTimeCallback(tseries, affect!)
+dosetimes = [5e3, 150e3]
+affect!(integrator) = integrator.u[12] += 0.100
+cb = PresetTimeCallback(dosetimes, affect!)
 
 #Set up the problem
 prob = SDEProblem(SAC_ODE, noise1D, u0, tspan, p0)
-@time sol = solve(prob, SOSRI(), reltol = 2e-2, abstol = 2e-2, progress = true, progress_steps = 1)
+@time sol = solve(prob, SOSRI(), reltol = 2e-2, abstol = 2e-2, 
+    tstops = dosetimes, callback = cb, 
+    progress = true, progress_steps = 1
+)
 
 # [Plot the solution]_________________________________________________________________________________________________________#
 fig1 = Figure(size = (1800, 800))
@@ -48,9 +43,9 @@ ax2b = Axis(fig1[2,2], title = "Na Gating (Mt)")
 ax3b = Axis(fig1[3,2], title = "Na Close (Ht)")
 
 ax1c = Axis(fig1[1,3], title = "Calcium (Ct)")
-ax4d = Axis(fig1[2,3], title = "Dopamine (Dt)")
-ax2c = Axis(fig1[3,3], title = "cAMP (At)")
-ax3c = Axis(fig1[4,3], title = "TREK (Bt)")
+ax2c = Axis(fig1[2,3], title = "Dopamine (Dt)")
+ax3c = Axis(fig1[3,3], title = "cAMP (At)")
+ax4c = Axis(fig1[4,3], title = "TREK (Bt)")
 
 ax1d = Axis(fig1[1,4], title = "ACh (Et)")
 ax2d = Axis(fig1[2,4], title = "GABA (It)")
@@ -59,15 +54,16 @@ ax3d = Axis(fig1[3,4], title = "Glutamate (Gt)")
 Time = sol.t
 lines!(ax1a, Time, map(t -> sol(t)[1], Time))
 lines!(ax2a, Time, map(t -> sol(t)[2], Time))
-lines!(ax3a, Time, map(t -> sol(t)[13], Time))
+lines!(ax3a, Time, map(t -> sol(t)[14], Time))
 
 lines!(ax1b, Time, map(t -> sol(t)[3], Time))
 lines!(ax2b, Time, map(t -> sol(t)[4], Time))
 lines!(ax3b, Time, map(t -> sol(t)[5], Time))
 
 lines!(ax1c, Time, map(t -> sol(t)[6], Time))
-lines!(ax2c, Time, map(t -> sol(t)[7], Time))
-lines!(ax3c, Time, map(t -> sol(t)[8], Time))
+lines!(ax2c, Time, map(t -> sol(t)[12], Time))
+lines!(ax3c, Time, map(t -> sol(t)[7], Time))
+lines!(ax4c, Time, map(t -> sol(t)[8], Time))
 
 lines!(ax1d, Time, map(t -> sol(t)[9], Time))
 lines!(ax2d, Time, map(t -> sol(t)[10], Time))
