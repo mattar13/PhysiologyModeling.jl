bfc8b8d1a7c72cecb8aaca8409700c30b9fe91c3
mattar13 <mtarchick13@gmail.com>
2025-05-19 12:32:14 -0700

    Refactor I_stim function to use parameters for stimulus configuration; add grid_discretization.jl file.

--- src/DopamineModels/auxillary_functions.jl
+++ src/DopamineModels/auxillary_functions.jl
@@ -1,8 +1,8 @@
 # external stimulus current (spike train or arbitrary function)
-function I_stim(t)
+function I_stim(t; stim_start = 100, stim_end = 400, stim_amplitude = 200, pulse_width = 5)
     # 5 ms square pulses at 20 Hz between 100–400 ms
-    if 100 <= t <= 400 && mod(t, 50) < 5            # 20 Hz
-        return 200.0  # pA
+    if stim_start <= t <= stim_end && mod(t, 50) < pulse_width            # 20 Hz
+        return stim_amplitude  # pA
     else
         return 0.0
     end
